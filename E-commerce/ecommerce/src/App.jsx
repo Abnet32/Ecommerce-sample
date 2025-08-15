040cb07fcc9eb8aab85a23d67b7d58c4d317a475
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-08-14 21:19:16 -0400

    Update App.jsx

--- E-commerce/ecommerce/src/App.jsx
+++ E-commerce/ecommerce/src/App.jsx
@@ -1,10 +1,12 @@
 import "./index.css";
 import Header from "./Components/Header/Header";
+import Body from "./Components/Body/Body";
 import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
       <Header />
+      <Body />
       <Footer />
     </>
   );
